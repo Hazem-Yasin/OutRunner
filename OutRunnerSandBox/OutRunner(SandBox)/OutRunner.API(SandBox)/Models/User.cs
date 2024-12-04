7fe11d6c6c4f88a9627ef03430f3ed73d7827948
Hazem Yasin <hazem.yasin.1416@gmail.com>
2024-12-05 01:10:49 +0200

    had some fun in the sandbox

---
+++ OutRunnerSandBox/OutRunner(SandBox)/OutRunner.API(SandBox)/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OutRunner.API_SandBox_.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+        public string UserPassword { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public float Height { get; set; }
+        public float Weight { get; set; }
+        public string ProfilePictureURL { get; set; }
+        public DateTime JoinDate { get; set; }
+
+    }
+}
---
+++ OutRunnerSandBox/OutRunner(SandBox)/OutRunner.API(SandBox)/Models/Walk.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace OutRunner.API_SandBox_.Models
+{
+    public class Walk
+    {
+        public int WalkId {get; set;}
+        public int UserId {get; set;}
+        public DateTime StartTime {get; set;}
+        public DateTime EndTime { get; set; } 
+        public float Distance { get; set; }
+        public TimeSpan Duration { get; set; }
+        public float AverageSpeed { get; set; }
+        public float CaloriesBurned { get; set; }
+        public string Route { get; set; }
+
+    }
+}
